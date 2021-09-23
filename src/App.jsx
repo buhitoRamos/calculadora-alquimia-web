1c9628f727268d7ce82d5babe8d446a40e76f369
buhitoRamos <martinbj99@hotmail.com>
2021-09-22 23:45:11 -0300

    feat(css, app.js): update style

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.css'
 import React, { useState } from 'react'
 
 const App = ()=> {
-  const [isAddAroms, setIsAddAroms]= useState(false)
   const [aromsName, setAromsName] = useState(
-    ["AROMA 1", "AROMA 2", "AROMA 3", "AROMA 4", "AROMA 5", "AROMA 6", "AROMA 7", "AROMA 8", "AROMA 9", "AROMA 10"]
+    [{aroma:"AROMA 1"}, {aroma:"AROMA 2"}, {aroma:"AROMA 3"}]
     );
 
-  const _addMoreAroms = (isAddAroms) => {
-    setIsAddAroms(!isAddAroms)
-    console.log(isAddAroms)
+  const _addMoreAroms = () => {
+    const aroma="AROMA 11"
+   setAromsName({...aromsName,
+    aroma
+  });
+ 
   };
 
 
@@ -19,8 +21,8 @@ const App = ()=> {
         aromsName.map(aroma =>
           
 
-          <div className="input-group mb-3" id={aroma}>
-            <span className="input-group-text" id="basic-addon1">{aroma}</span>
+          <div className="input-group mb-3" id={aroma.aroma}>
+            <span className="input-group-text" id="basic-addon1">{aroma.aroma}</span>
             <input type="text" className="form-control" placeholder="ingrese porcentaje" aria-label="Username" aria-describedby="basic-addon1" />
           </div>
 
@@ -52,7 +54,7 @@ const App = ()=> {
         <div className="position-relative">
           <button type="button" 
           className="btn btn-primary position-absolute top-0 start-50 translate-middle-x"
-          onClick={()=> _addMoreAroms(isAddAroms)}>
+          onClick={()=> _addMoreAroms()}>
             Agregar mas Aromas</button>
         </div>
       </div>
