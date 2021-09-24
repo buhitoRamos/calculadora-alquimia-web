72b0fcbb4ddce752fb5ed8e2cab65aabfd249707
buhitoRamos <martinbj99@hotmail.com>
2021-09-23 23:35:35 -0300

    feat(app.jsx): add a onChange function for scent input

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,77 @@
 import './App.css'
 import React, { useState } from 'react'
 
-const App = ()=> {
-  const [aromsName, setAromsName] = useState([{value:"AROMA", id:0}]);
+const App = () => {
+  const [aroms, setAroms] = useState([{ name: "", index: 0, value: "" }]);
 
   const _addAroms = () => {
-    const id=aromsName.length;
-    const aroma = {value:"AROMA", id};
-    setAromsName([ ...aromsName, aroma])
+    const index = aroms.length;
+    const aroma = { value: "", index };
+    setAroms([...aroms, aroma])
   };
+  
   const _deleteAroms = () => {
-   const idToDeleted = aromsName.length -1;
-   const newAromsName = aromsName.filter(({id}) => id !== idToDeleted);
-   setAromsName(newAromsName)
+    const idToDeleted = aroms.length - 1;
+    const newAroms = aroms.filter(({ index }) => index !== idToDeleted);
+    setAroms(newAroms)
   }
 
+  const _handleChange = (aroma, event) => {
+    const { index } = aroma;
+    const { value } = event.target;
+    const {id} = event.target
+    let newArom  = aroms.map(function(arom) {
+      if(arom.index === index) {
+        id === "label" ? arom.name = value : arom.value = value;
+      }
+      return arom
+    });
+    setAroms(newArom);
+  }
 
   const _renderAroms = () => {
-      return (
-        aromsName.map(aroma =>
-          
-          <div className="input-group mb-3" id={aroma.value}>
-            <span className="input-group-text" id="basic-addon1">{aroma.value}</span>
-            <input type="text" className="form-control" placeholder="ingrese porcentaje" aria-label="Username" aria-describedby="basic-addon1" />
-          </div>
-        )
-      )
-    }
-
     return (
-      <div className="container">
-        <div className="input-group mb-3 pt-1">
-          <span className="input-group-text" id="basic-addon1">GLICERINA</span>
-          <input type="text" className="form-control" placeholder="ingrese porcentaje" aria-label="Username" aria-describedby="basic-addon1" />
+      aroms.map(aroma =>
+
+        <div className="input-group mb-3" id={aroma.value}>
+          <input className="input-group-text" id="label"
+            placeholder="aroma" defaultValue={aroma.name} onChange={(ev) => _handleChange(aroma, ev)} />
+          <input type="text" className="form-control"
+            placeholder="ingrese porcentaje" aria-label="input"
+            aria-describedby="ingreso de aroma"
+            defaultValue={aroma.value} 
+            onChange={(ev) => _handleChange(aroma, ev)}/>
         </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="basic-addon1">PROPILEN</span>
-          <input type="text" className="form-control" placeholder="ingrese porcentaje" aria-label="Username" aria-describedby="basic-addon1" />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="basic-addon1">NICOTINA</span>
-          <input type="text" className="form-control" placeholder="ingrese porcentaje" aria-label="Username" aria-describedby="basic-addon1" />
-        </div>
-        <div className="overFlow">
-{
-            _renderAroms()
-          }
-        </div>
-          
-        
-        <div className="mp-2">
-          <span className=" row align-items-center mr-50">
+      )
+    )
+  }
+
+  return (
+    <div className="container">
+      <p className="text-center bg-primary  text-white">Ingreso de aromas y porcentajes</p>
+      <div className="input-group mb-3 pt-1">
+        <span className="input-group-text" id="basic-addon1">GLICERINA</span>
+        <input type="text" className="form-control" placeholder="ingrese porcentaje" aria-label="Username" aria-describedby="basic-addon1" />
+      </div>
+      <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">PROPILEN</span>
+        <input type="text" className="form-control" placeholder="ingrese porcentaje" aria-label="Username" aria-describedby="basic-addon1" />
+      </div>
+      <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">NICOTINA</span>
+        <input type="text" className="form-control" placeholder="ingrese porcentaje" aria-label="Username" aria-describedby="basic-addon1" />
+      </div>
+      <div className="overFlow">
+        {
+          _renderAroms()
+        }
+      </div>
+
+
+      <div className="mp-2">
+        <span className=" row align-items-center mr-50">
           <button type="button"
-           className="btn btn-primary"
+            className="btn btn-primary"
             onClick={() => _addAroms()}>
             Agregar aroma
           </button>
@@ -61,12 +80,12 @@ const App = ()=> {
             onClick={() => _deleteAroms()}>
             Eliminar aroma
           </button>
-          </span>
-        </div>
+        </span>
       </div>
-    );
-  
-  
+    </div>
+  );
+
+
 }
 
 export default App;
