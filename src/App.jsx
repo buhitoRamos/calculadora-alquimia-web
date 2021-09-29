4e9109f8b5262a186a5c2021c1f555e23135b4c7
buhitoRamos <martinbj99@hotmail.com>
2021-09-28 22:45:09 -0300

    feat(App.jsx, Aroms.jsx): move tags to Aroms component and add validations

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,68 @@
 import './App.css'
 import React, { useState } from 'react'
-import Aroms from"./components/Aroms/Aroms"
+import Aroms from "./components/Aroms/Aroms"
 
 const App = () => {
   const [aroms, setAroms] = useState([{ name: "", index: 0, value: "" }]);
   const [form, setForm] = useState([
-    {name: "GLICERINA", value:""}, 
-    {name:"PROPILEN", value: ""},
-    {name: "NICOTINA", value: ""}
-    ])
+    { name: "GLICERINA", value: "" },
+    { name: "PROPILEN", value: "" },
+    { name: "NICOTINA", value: "" }
+  ])
 
-  const _addAroms = () => {
+  const addAroms = () => {
     const index = aroms.length;
-    const aroma = { value: "", index };
+    const aroma = {name: "", index, value: ""  };
     setAroms([...aroms, aroma])
   };
-  
-  const _deleteAroms = () => {
+
+  const deleteAroms = () => {
     const idToDeleted = aroms.length - 1;
     const newAroms = aroms.filter(({ index }) => index !== idToDeleted);
     setAroms(newAroms)
   }
 
-  const _handleChange = (aroma, event) => {
+  const handleChange = (aroma, event) => {
     const { index } = aroma;
-    const { value } = event.target;
-    const {id} = event.target
-    let newArom  = aroms.map(function(arom) {
-      if(arom.index === index) {
-        id === "label" ? arom.name = value : arom.value = value;
+    const { value, id, maxLength } = event.target;
+    let newArom = aroms.map(function (arom) {
+      if (arom.index === index) {
+        if(id === "label") {
+          arom.name = value 
+        } else {
+          if(value.length < maxLength) {
+            arom.value = value
+          }
+        }
       }
       return arom
     });
     setAroms(newArom);
   }
-  const _handleFormChange = (ev) => {
-    const {id} = ev.target
-    const { value } = ev.target;
-    let newForm  = form.map(function(form) {
-      if(form.name === id) {
-        form.value = value
+  const handleFormChange = (ev) => {
+    const { id } = ev.target
+    const { value, maxLength } = ev.target;
+    let newForm = form.map(function (form) {
+      if (form.name === id) {
+       if(value.length < maxLength) {
+         form.value = value
+       } 
+        
       }
       return form
     });
     setForm(newForm)
   }
 
-  /*const _renderAroms = () => {
-    return (
-      aroms.map(aroma =>
-
-        <div className="input-group mb-3" id={aroma.index}>
-          <input className="input-group-text" id="label"
-            placeholder="aroma" defaultValue={aroma.name} onChange={(ev) => _handleChange(aroma, ev)} />
-          <input type="text" className="form-control"
-            placeholder="ingrese porcentaje" aria-label="input"
-            aria-describedby="ingreso de aroma"
-            defaultValue={aroma.value} 
-            onChange={(ev) => _handleChange(aroma, ev)}/>
-        </div>
-      )
-    )
-  }*/
-
 
   return (
     <div className="container">
-      <p className="text-center bg-primary  text-white">Ingreso de aromas y porcentajes</p>
-      <div className="input-group mb-3 pt-1">
-        <span className="input-group-text" id="basic-addon1">{form[0].name}</span>
-        <input type="number" className="form-control" placeholder="ingrese porcentaje" id={form[0].name} 
-        aria-describedby="glicerina" defaultValue={form[0].value}
-        onChange={_handleFormChange} />
-      </div>
-      <div className="input-group mb-3">
-        <span className="input-group-text" id="basic-addon1">{form[1].name}</span>
-        <input type="number" className="form-control" placeholder="ingrese porcentaje" id={form[1].name} 
-        aria-describedby="propilenglicol"
-        onChange={_handleFormChange} />
-      </div>
-      <div className="input-group mb-3">
-        <span className="input-group-text" id="basic-addon1">{form[2].name}</span>
-        <input type="number" className="form-control" placeholder="ingrese porcentaje" id={form[2].name}
-         aria-describedby="nicotina"
-         onChange={_handleFormChange} />
-      </div>
-      <Aroms aroms={aroms} handleChange={_handleChange} />
-
-
-      <div className="mp-2">
-        <span className=" row align-items-center mr-50">
-          <button type="button"
-            className="btn btn-primary"
-            onClick={() => _addAroms()}>
-            Agregar aroma
-          </button>
-          <button type="button"
-            className="btn btn-danger mt-1"
-            onClick={() => _deleteAroms()}>
-            Eliminar aroma
-          </button>
-        </span>
-      </div>
+      <Aroms aroms={aroms}
+        form={form}
+        handleChange={handleChange}
+        handleFormChange={handleFormChange}
+        addAroms={addAroms}
+        deleteAroms={deleteAroms} />
     </div>
   );
 
--- src/components/Aroms/Aroms.jsx
+++ src/components/Aroms/Aroms.jsx
@@ -1,32 +1,74 @@
 import React from 'react'
 
 
-
-
-  const _renderAroms = (aroms, handleChange) => {
-    return (
-      aroms.map(aroma =>
-
-        <div className="input-group mb-3" id={aroma.index}>
-          <input className="input-group-text" id="label"
-            placeholder="aroma" defaultValue={aroma.name} onChange={(ev) => handleChange(aroma, ev)} />
-          <input type="text" className="form-control"
-            placeholder="ingrese porcentaje" aria-label="input"
-            aria-describedby="ingreso de aroma"
-            defaultValue={aroma.value}
-            onChange={(ev) => handleChange(aroma, ev)} />
-        </div>
-      )
+const _renderAroms = (aroms, handleChange) => {
+  return (
+    aroms.map(aroma =>
+      <div className="input-group mb-3" id={aroma.index} key={aroma.index}>
+        <input className="input-group-text" id="label"
+          placeholder="aroma" defaultValue={aroma.name} onChange={(ev) => handleChange(aroma, ev)} />
+        <input className="form-control"
+          placeholder="ingrese porcentaje" aria-label="input"
+          aria-describedby="ingreso de aroma"
+          type="number"
+          maxLength="3"
+          value={aroma.value}
+          onChange={(ev) => handleChange(aroma, ev)} />
+      </div>
     )
-  }
+  )
+}
 
 
-  const Aroms = ({aroms, handleChange}) => (
-    <section className="overFlow">
+
+const Aroms = ({ aroms, form, handleChange, handleFormChange, addAroms, deleteAroms }) => (
+  <section>
+    <p className="text-center bg-primary  text-white">Ingreso de aromas y porcentajes</p>
+    <div className="input-group mb-3 pt-1">
+      <span className="input-group-text" id="basic-addon1">{form[0].name}</span>
+      <input type="number" className="form-control" placeholder="ingrese porcentaje" id={form[0].name}
+        aria-describedby="glicerina"
+        maxLength="3"
+        value={form[0].value}
+        onChange={handleFormChange} />
+    </div>
+    <div className="input-group mb-3">
+      <span className="input-group-text" id="basic-addon1">{form[1].name}</span>
+      <input type="number" className="form-control" placeholder="ingrese porcentaje" id={form[1].name}
+        aria-describedby="propilenglicol"
+        maxLength="3"
+        value={form[1].value}
+        onChange={handleFormChange} />
+    </div>
+    <div className="input-group mb-3">
+      <span className="input-group-text" id="basic-addon1">{form[2].name}</span>
+      <input type="number" className="form-control" placeholder="ingrese porcentaje" id={form[2].name}
+        aria-describedby="nicotina"
+        maxLength="3"
+        value={form[2].value}
+        onChange={handleFormChange} />
+    </div>
+
+    <div className="overFlow">
       {
         _renderAroms(aroms, handleChange)
       }
-    </section>
-  )
+    </div>
+    <div className="mp-2">
+      <span className=" row align-items-center mr-50">
+        <button type="button"
+          className="btn btn-primary"
+          onClick={() => addAroms()}>
+          Agregar aroma
+        </button>
+        <button type="button"
+          className="btn btn-danger mt-1"
+          onClick={() => deleteAroms()}>
+          Eliminar aroma
+        </button>
+      </span>
+    </div>
+  </section>
+)
 
 export default Aroms;
