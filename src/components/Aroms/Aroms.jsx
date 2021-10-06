bc6a538642e087869d24211bdaef9860dfae387f
martin.lopez3.contractor.gdevtools.com <martin.lopez3.contractor@gdevtools.com>
2021-10-06 18:42:31 -0300

    feat(Aroms.jsx, css): add style

--- src/components/Aroms/Aroms.jsx
+++ src/components/Aroms/Aroms.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import "./aroms.css"
 
 
 const _renderAroms = (aroms, handleChange) => {
   return (
     aroms.map(aroma =>
       <div className="input-group mb-3" id={aroma.index} key={aroma.index}>
-        <input className="input-group-text" id="label"
+        <input className="input-group-text max-width-lbl" id="label"
           placeholder="aroma" defaultValue={aroma.name} onChange={(ev) => handleChange(aroma, ev)} />
-        <input className="form-control"
-          placeholder="ingrese porcentaje" aria-label="input"
+        <input className="form-control max-width-input"
+          placeholder="%" aria-label="input"
           aria-describedby="ingreso de aroma"
           type="number"
           maxLength="3"
@@ -22,11 +23,11 @@ const _renderAroms = (aroms, handleChange) => {
 
 
 const Aroms = ({ aroms, form, handleChange, handleFormChange, addAroms, deleteAroms }) => (
-  <section>
+  <section className="max-width-section">
     <p className="text-center bg-primary  text-white">Ingreso de aromas y porcentajes</p>
     <div className="input-group mb-3 pt-1">
       <span className="input-group-text" id="basic-addon1">{form[0].name}</span>
-      <input type="number" className="form-control" placeholder="ingrese porcentaje" id={form[0].name}
+      <input type="number" className="form-control" placeholder="%" id={form[0].name}
         aria-describedby="glicerina"
         maxLength="3"
         value={form[0].value}
@@ -34,7 +35,7 @@ const Aroms = ({ aroms, form, handleChange, handleFormChange, addAroms, deleteAr
     </div>
     <div className="input-group mb-3">
       <span className="input-group-text" id="basic-addon1">{form[1].name}</span>
-      <input type="number" className="form-control" placeholder="ingrese porcentaje" id={form[1].name}
+      <input type="number" className="form-control" placeholder="%" id={form[1].name}
         aria-describedby="propilenglicol"
         maxLength="3"
         value={form[1].value}
@@ -42,7 +43,7 @@ const Aroms = ({ aroms, form, handleChange, handleFormChange, addAroms, deleteAr
     </div>
     <div className="input-group mb-3">
       <span className="input-group-text" id="basic-addon1">{form[2].name}</span>
-      <input type="number" className="form-control" placeholder="ingrese porcentaje" id={form[2].name}
+      <input type="number" className="form-control" placeholder="%" id={form[2].name}
         aria-describedby="nicotina"
         maxLength="3"
         value={form[2].value}
