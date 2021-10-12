5e99cf760d2c292f9570d01e07e9a3378081799a
martin.lopez3.contractor.gdevtools.com <martin.lopez3.contractor@gdevtools.com>
2021-10-12 18:07:34 -0300

    feat(OhmsCalculator): add amperCalculator and ohmsCalculator

--- src/components/Ohms-calculator/OhmsCalculator.jsx
+++ src/components/Ohms-calculator/OhmsCalculator.jsx
@@ -27,6 +27,40 @@ const OhmsCalculator = () => {
 
     setForm(newForm);
   }
+  const _amperCalculate =(volt, ohms, watt) => {
+      let aux = ""
+      if(volt > 0 && ohms > 0) {
+        aux = volt/ohms;   
+      } else if(volt > 0 && watt > 0) {
+        aux = watt/volt;
+      } else if(watt > 0 && ohm > 0){
+          aux = Math.sqrt(watt / ohms);
+      }
+      
+    return aux
+  }
+  const _ohmsCalculate = (volt, watt, amp) => {
+      let aux =""
+      if(volt > 0 && amp > 0 ){
+        aux = volt/amp
+      } else if (volt > 0 && watt > 0 ) {
+          aux = vot* volt / watt;
+      } else if (watt > 0 && amp > 0) {
+          aux =watt /(amp*amp);
+      }
+      return aux;
+  }
+  const calculate = () => {
+      const volt = form[0].value;
+      const watt = form[1].value;
+      const ohms = form[2].value;
+      const amp = form[3].value;
+      let text = _amperCalculate(volt, ohms, watt);
+      text = `/n ${text} ${_ohmsCalculate}`;
+    
+      
+
+  }
   const clear = () => {
     
     setForm(
