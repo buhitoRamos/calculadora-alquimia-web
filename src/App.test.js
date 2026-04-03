b534fa07a9839e4fabf94f69a3b4159ef3257ed7
buhoRamos <martin_bj99@hotmail.com>
2026-04-02 21:27:18 -0300

    remove test

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the calculator', () => {
-  render(<App />);
-  expect(screen.getByText(/Calculadora Alquimia/i)).toBeInTheDocument();
-});
